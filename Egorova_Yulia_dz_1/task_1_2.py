bfc0b5a328b809eb6abb8e3c42fc70b2f429c2a5
YuliaE <yulia2000.egorova@yandex.ru>
2021-11-25 20:53:06 +0300

    добавила решение задания 2

--- Egorova_Yulia_dz_1/task_1_2.py
+++ Egorova_Yulia_dz_1/task_1_2.py
@@ -1,33 +1,28 @@
-numbers=[]
+numbers = []
 
-for number in range(1,1000,2):
-    number=number**3
+for number in range(1, 1000, 2):
+    number = number ** 3
     numbers.append(number)
 print(numbers)
 
-sum_range=0
-sum_range_17=0
+sum_range = 0
+sum_range_17 = 0
 for number in numbers:
     sum = 0
-    num=number
-    while num !=0:
-        sum=(sum+(num%10))
-        num = num//10
-    if sum%7==0:
-        sum_range=sum_range+number
+    num = number
+    while num != 0:
+        sum = (sum + (num % 10))
+        num = num // 10
+    if sum % 7 == 0:
+        sum_range = sum_range + number
 print(sum_range, 'сумма чисел, сумма цифр, которых делится на 7')
 for number in numbers:
     sum = 0
-    number+=17
-    num=number
-    while num !=0:
-        sum=(sum+(num%10))
-        num = num//10
-    if sum%7==0:
-        sum_range_17+=number
+    number += 17
+    num = number
+    while num != 0:
+        sum = (sum + (num % 10))
+        num = num // 10
+    if sum % 7 == 0:
+        sum_range_17 += number
 print(sum_range_17, 'сумма чисел после прибавления 17')
-
-    
-
-
-
