09d524dc9610b65cec3c0c3d6913a369494c980e
YuliaE <yulia2000.egorova@yandex.ru>
2021-12-24 17:38:29 +0300

    добавила решение задания 2

--- Egorova_Yulia_dz_9/task_9_2.py
+++ Egorova_Yulia_dz_9/task_9_2.py
@@ -1,9 +1,9 @@
 class Road:
     def __init__(self,length,width):
-        self.length = int(length)
-        self.width = int(width)
+        self._length = int(length)
+        self._width = int(width)
     def count_mass(self):
-        self.mass=self.length*self.width*25*5//1000
+        self.mass=self._length*self._width*25*5//1000
         print(f'масса асфальта для вашей дороги равна: {self.mass} т')
-a = Road(20,5000)
+a = Road(25,5000)
 a.count_mass()
